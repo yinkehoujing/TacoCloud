a27ade2b9d7b7aff9f3e4f3c585a7c1f67bc7eea
Grl <abc.d>
2024-08-26 18:30:47 +0800

    fir-implement

---
+++ src/test/java/com/grl/tacoproj/TacoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grl.tacoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
